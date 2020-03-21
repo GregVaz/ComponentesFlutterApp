058730563edca38f86103769a8ce8e549d8f545f
GregVaz <grvazquezya@ittepic.edu.mx>
2020-03-21 11:05:46 -0700

    Implementacion de FutureBuilder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,22 +15,55 @@ class HomePage extends StatelessWidget {
 
   Widget _lista() {
 
-    print(menuProvider.opciones);
+    // print(menuProvider.opciones);
+    
+    /*menuProvider.cargarData().then( (opciones) {
+        print('Lista');
+        print(opciones);
+       });
+      Si decidieramos hacer lo anterior, para obtener la data de donde la consumamos, 
+      y colocamos el metodo 
+        return ListView(
+          children: _listaItems(),
+        );
+      dentro de nuestro cargarData(). La aplicacion, pareceria congelada, por lo que la
+      respuesta para resolve esto es a traves del FutureBuilder de Flutter 
+    */
+    // Utilizaremos el Future Builder
+    return FutureBuilder(
+        future: menuProvider.cargarData(),
+        initialData: [], // Data que se cargara en espera de la informacion en consumo
+        builder: ( context, AsyncSnapshot<List<dynamic>> snapshot ){
+          
+          // print('builder');
+          // print(snapshot.data);
 
-    return ListView(
-      children: _listaItems(),
-    );  
+          return ListView(
+            children: _listaItems( snapshot.data ),
+          );  
+
+        },
+    );
       
   }
       
-  List<Widget> _listaItems() {
-    return [
-      ListTile( title: Text('Hola mundo') ),
-      Divider(),
-      ListTile( title: Text('Hola mundo') ),
-      Divider(),
-      ListTile( title: Text('Hola mundo') ),
-      Divider(),
-    ];
+  List<Widget> _listaItems( List<dynamic> data ) {
+
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text( opt['texto'] ),
+        leading: Icon( Icons.person_pin, color: Colors.blue ),
+        trailing: Icon( Icons.keyboard_arrow_down, color: Colors.blue ),
+        onTap: () {},
+      );
+
+      opciones..add( widgetTemp )
+              ..add( Divider() );
+    });
+
+    return opciones;
+
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle; // Servicio para consumir los recursos
 
 class _MenuProvider {
@@ -6,16 +8,17 @@ class _MenuProvider {
   List<dynamic> opciones = [];
 
   _MenuProvider() {
-    cargarData();
+    // cargarData();
   }
 
-  cargarData() {
+  Future<List<dynamic>> cargarData() async {
 
-    rootBundle.loadString('data/menu_opts.json')
-    .then( (data) {
-      print(data);
-    });
+    final resp = await rootBundle.loadString('data/menu_opts.json');
+    
+    Map dataMap = json.decode(resp);
+    opciones = dataMap['rutas'];
 
+    return opciones;
   }
 
 }
